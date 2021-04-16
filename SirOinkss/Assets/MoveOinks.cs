4fe27623908e751770a81e4f2b3dd067a1973f64
Wartoffe <63714418+Wartoffe@users.noreply.github.com>
2021-04-16 23:23:38 +0200

    Clean up and set up
    Cleaned up the file
    Cleaned up on movement
    Added a Script for collision (works on half the assets for now

---
+++ SirOinkss/Assets/MoveOinks.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class MoveOinks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+    public float speed = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float yDirection = Input.GetAxis("Vertical");
+
+        Vector2 moveDirection = new Vector2(xDirection, yDirection).normalized;
+
+
+        //Move(moveDirection);
+        transform.position += (Vector3)moveDirection*speed;
+    }
+
+    private void Move(Vector3 direction)
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition += direction * speed * Time.deltaTime;
+        transform.position = currentPosition;
+    }
+}
